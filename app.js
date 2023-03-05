85acf53f9bf3de51a2e606b7b9f94842d896aca4
anaomart <anaomart@gmail.com>
2023-03-05 04:36:29 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,144 @@
+const cardElement = document.querySelector('.cards');
+
+// set data in localStorage (note : data is separate file (data.js))
+if (!localStorage.getItem('products')) {
+    localStorage.setItem('products', JSON.stringify(products))
+}
+products = JSON.parse(localStorage.getItem('products')) || products
+
+function updateProducts() {
+    localStorage.setItem('products', JSON.stringify(products))
+}
+
+function getLocalStorageProducts() {
+    return JSON.parse(localStorage.getItem('products'));
+}
+
+function getCartProducts() {
+    return JSON.parse(localStorage.getItem('cart'));
+}
+
+function getCartProductsLength() {
+    return getCartProducts() ? Object.keys(getCartProducts()).length : 0;
+}
+// display a single card 
+function displayCard(product) {
+    return (
+        `<div class="card" data-index=${product.id}>
+                <div class="image">
+                    <img src=${product.product_image} />
+                </div>
+                <div class="card-info">
+                    <p class="product-name">${product.product_name} </p>
+                    <span class="product-price">${product.product_price}$</span>
+                </div>
+                <div class="card-buttons">
+                <button class='cartButton' onclick="addOrRemoveFromCart(${product.id})"> 
+                ${
+                    product.added_to_cart ? 'Remove from cart' : 'Add to cart'
+                }
+                </button>
+                    <button type='button' onclick="displayQuickView(${product.id})"> Quick view</button>
+                </div>
+        </div>`
+    )
+}
+// display products list 
+function displayProducts(products) {
+    cardElement.innerHTML = products.map(product => {
+        return displayCard(product)
+    }).join('')
+}
+
+// cart List 
+const cartList = document.querySelector('.cartList');
+
+function displayCartList() {
+    if (cartList.style.display == 'block') {
+        cartList.style = 'display:none;'
+    } else {
+        cartList.style = 'display:block;';
+    }
+}
+// Cart indicator
+const indicator = document.querySelector('.indicator');
+
+function updateIndicator() {
+    const productsLength = getCartProductsLength();
+    indicator.textContent = productsLength || 0;
+}
+
+// add and remove from  Cart ;
+function addOrRemoveFromCart(id) {
+    let product = products[id];
+    let cart = getCartProducts();
+
+    if (cart) {
+        if (JSON.parse(localStorage.getItem('cart'))[id]) {
+            removeFromCart(id)
+            return;
+        }
+        cart[id] = product;
+        localStorage.setItem('cart', JSON.stringify(cart));
+    } else {
+        const cartItem = {};
+        cartItem[id] = product;
+        localStorage.setItem('cart', {});
+        localStorage.setItem('cart', JSON.stringify(cartItem));
+    }
+    products[id].added_to_cart = true;
+    reRender()
+}
+
+// remove from cart
+function removeFromCart(id) {
+    const cartProducts = getCartProducts();
+    delete cartProducts[id]
+    localStorage.setItem('cart', JSON.stringify(cartProducts));
+    products = getLocalStorageProducts();
+    products[id].added_to_cart = false;
+    reRender();
+}
+
+function displayItemsOnCartList() {
+    let items = getCartProducts();
+    const elem = document.createElement("div");
+    cartList.innerHTML = '';
+    Object.keys(items).forEach(function(key) {
+        let item = `
+        <div class="item">
+        <div class="image">
+            <img src=${items[key]?.product_image} />
+        </div>
+        <div>
+            <p class="product-name">${items[key]?.product_name} </p>
+            <span class="product-price">${items[key]?.product_price} $</span>
+            <button onclick="removeFromCart(${items[key]?.id});displayCartList()">Remove </button>
+        </div>
+    </div>
+        `;
+        elem.innerHTML += item
+    });
+    cartList.appendChild(elem);
+}
+// update page information after a change 
+function reRender() {
+    updateProducts();
+    updateIndicator();
+    displayItemsOnCartList()
+    displayProducts(getLocalStorageProducts());
+}
+// quick-view 
+const overlay = document.querySelector('#overlay');
+const quickView = document.querySelector('#quick-view')
+overlay.onclick = () => overlay.style.display = 'none'
+
+function displayQuickView(id) {
+    console.log("clicks")
+    overlay.style.display = 'block'
+    quickView.innerHTML = displayCard(products[id]);
+}
+
+displayProducts(getLocalStorageProducts());
+displayItemsOnCartList()
+updateIndicator();
---
+++ data.js
@@ -0,0 +1,41 @@
+let products = [{
+        id: 0,
+        product_name: "Mens Casual Premium Slim Fit T-Shirts",
+        product_price: 109.95,
+        product_image: "https://m.media-amazon.com/images/I/61-oKLAgcyL._AC_UL1500_.jpg",
+        added_to_cart: false,
+    },
+    {
+        id: 1,
+        product_name: "Mens Cotton Jacket",
+        product_price: 55.99,
+        product_image: "https://m.media-amazon.com/images/I/41yvByo17EL._AC_.jpg",
+        added_to_cart: false,
+    },
+    {
+        id: 2,
+        product_name: "Mens Casual Slim Fit",
+        product_price: 15.99,
+        product_image: "https://m.media-amazon.com/images/I/71wesfhPIfL._AC_UL1500_.jpg",
+        added_to_cart: false,
+    }, {
+        id: 3,
+        product_name: "Solid Gold Petite Micropave",
+        product_price: 168,
+        product_image: "https://m.media-amazon.com/images/I/51fBSFgO3CS._AC_UL1500_.jpg",
+        added_to_cart: false,
+    }, {
+        id: 4,
+        product_name: "White Gold Plated Princess",
+        product_price: 9.99,
+        product_image: "https://m.media-amazon.com/images/I/71mq5Zqr6uL._AC_UL1500_.jpg",
+        added_to_cart: false,
+    }, {
+        id: 5,
+        product_name: "Pierced Owl Rose Gold Plated Stainless Steel Double",
+        product_price: 109.95,
+        product_image: "https://m.media-amazon.com/images/I/61rc+YSZslL._AC_UL1000_.jpg",
+        added_to_cart: false,
+    }
+]
+exports = products;
